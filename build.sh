aed576e26fbb1a3150b1dbec0d3e8f5615b5adb4
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2013-04-09 01:18:09 -0400

    Mangle import library to fix bad link with link.exe /opt:ref

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #
 # A script for building OpenSlide and its dependencies for Windows
 #
-# Copyright (c) 2011-2012 Carnegie Mellon University
+# Copyright (c) 2011-2013 Carnegie Mellon University
 # All rights reserved.
 #
 # This script is free software: you can redistribute it and/or modify it
@@ -404,6 +404,31 @@ build_one() {
             make check
         fi
         make install
+        # Rename import library members to fix problems with /OPT:REF
+        # http://sourceware.org/ml/binutils/2010-03/msg00126.html
+        # http://sourceware.org/ml/binutils/2010-03/msg00175.html
+        # http://lists.andrew.cmu.edu/pipermail/openslide-users/2013-April/000597.html
+        local tempdir implib dynlib
+        tempdir="${root}/lib/implib"
+        implib="../libopenslide.dll.a"
+        dynlib="libopenslide-0.dll"
+        mkdir "$tempdir"
+        pushd "$tempdir" >/dev/null
+        # If the archive contains <= 3 unique member names, assume it does not
+        # need to be patched.
+        if [ $(${build_host}-ar t "$implib" | sort -u | wc -l) -gt 3 ]; then
+            ${build_host}-ar x "$implib"
+            rm "$implib"
+            for file in *
+            do
+                mv "$file" "$dynlib"
+                ${build_host}-ar q "$implib" "$dynlib"
+                rm "$dynlib"
+            done
+            chmod +x "$implib"
+        fi
+        popd >/dev/null
+        rmdir "$tempdir"
         ;;
     openslidejava)
         do_configure \
