openslide/openslide-bin

commit ecc9a6149afe03463fb69c82a0d048b5dc826835

Dockerfile.builder: drop Ant
It was only used for OpenSlide Java, which has switched to Meson.

Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

Dockerfile.builder
@@ -1,4 +1,4 @@
 FROM registry.fedoraproject.org/fedora:37
-RUN dnf -y install ant bzip2 cmake gcc gettext git-core glib2-devel java \
+RUN dnf -y install bzip2 cmake gcc gettext git-core glib2-devel java \
     meson mingw{32,64}-gcc-c++ nasm wget xz zip && \
     dnf clean all

commit 25a732b12d48fde4e0c56da3a33412ea9fb9273f

Dockerfile.builder: re-add javac
Ant was pulling it in, but we need to be explicit now.

Fixes: ecc9a6149a ("Dockerfile.builder: drop Ant")
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

Dockerfile.builder
@@ -1,4 +1,4 @@
 FROM registry.fedoraproject.org/fedora:37
-RUN dnf -y install bzip2 cmake gcc gettext git-core glib2-devel java \
+RUN dnf -y install bzip2 cmake gcc gettext git-core glib2-devel java-devel \
     meson mingw{32,64}-gcc-c++ nasm wget xz zip && \
     dnf clean all