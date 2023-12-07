openslide/openslide-bin

commit c8c9c063a2a151b98a3b15d11984b9244ae63a9b

Generate sdist archive with `meson dist`
Reuse existing Meson functionality, rather than manually maintaining a
list of files to include in the archive.  As a consequence, rather than
shipping a nested compressed tarball for each dependency, ship the actual
unpacked source trees.

Meson supports tar.xz, tar.gz, and ZIP.  It's no longer necessary for the
sdist archive format to be natively supported by Windows, since Windows
builds now always use a Linux container.  ZIP is less efficient because
each file is compressed individually, so avoid it.  tar.xz would be ideal,
but PyPI requires tar.gz, and it would be nice to reuse the same sdist
archive when we add support for Python wheels.  The source archive
shouldn't be used too often, so avoid generating both tar.gz and tar.xz
archives and accept the lower compression ratio.

Embed the currently configured version number in the generated source
archive.  When building from a source archive, allow overriding the
version in the usual way; otherwise append "-local" to the embedded
version, if missing, to make it clear that the resulting bdist is a
rebuild.  This prevents builds of old sdists from using a version derived
from the current date.

Normally, each platform build only enables the dependencies needed for
that platform.  The Windows build currently enables all dependencies, but
that may not always be true.  Add a Meson option that enables all
dependencies, for use when generating sdist archives, so they all end up
in the tarball.

When working from an unpacked sdist archive, avoid purging subproject
sources, since those are now canonical.

Closes: https://github.com/openslide/openslide-bin/issues/159
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

.gitattributes
@@ -1,2 +1,10 @@
 # Shell scripts can't have CRLF line endings
 *.sh		eol=lf
+
+# Don't include Git/GitHub metadata and development tool configs in
+# sdist tarballs
+.gitattributes			export-ignore
+.gitignore			export-ignore
+/.github			export-ignore
+/.pre-commit-config.yaml	export-ignore
+/pyproject.toml			export-ignore

artifacts/postprocess-sdist.py
@@ -1,0 +1,52 @@
+#!/usr/bin/env python3
+#
+# Tools for building OpenSlide and its dependencies
+#
+# Copyright (c) 2023 Benjamin Gilbert
+# All rights reserved.
+#
+# This script is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License, version 2.1,
+# as published by the Free Software Foundation.
+#
+# This script is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this script. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from __future__ import annotations
+
+import os
+from pathlib import Path
+import sys
+
+# handle our own PYTHONPATH prepending, since meson can't set environment
+# variables for dist scripts
+# flake8 isn't happy about this
+sys.path.insert(0, os.environ['MESON_SOURCE_ROOT'])
+
+from common.argparse import TypedArgs  # noqa: E402
+from common.meson import meson_introspect  # noqa: E402
+
+
+class Args(TypedArgs):
+    introspect: str
+
+
+args = Args(
+    'postprocess-sdist', description='Modify sdist directory before packing.'
+)
+args.add_arg(
+    '-i', '--introspect', required=True, help='Meson introspect command'
+)
+args.parse()
+os.environ['MESONINTROSPECT'] = args.introspect
+dest = Path(os.environ['MESON_DIST_ROOT'])
+
+# pin openslide-bin version
+version: str = meson_introspect('projectinfo')['version']
+(dest / 'version').write_text(version + '\n')

.github/workflows/build.yml
@@ -68,7 +68,7 @@ permissions:
 
 jobs:
   sdist:
-    name: Source zip
+    name: Source tarball
     runs-on: ubuntu-latest
     container: ${{ inputs.windows_builder_repo_and_digest }}
     outputs:
@@ -116,15 +116,17 @@ jobs:
         with:
           key: build-packagecache
           path: subprojects/packagecache
-      - name: Build source zip
-        run: ./build.sh -p "${{ inputs.pkgver }}" sdist
+      - name: Build source tarball
+        run: |
+          git config --global --add safe.directory "$GITHUB_WORKSPACE"
+          ./build.sh -p "${{ inputs.pkgver }}" sdist
       - name: Prep artifact
         id: prep
         run: |
           artifact="openslide-windows-${{ inputs.pkgver }}"
           echo "artifact=$artifact" >> $GITHUB_OUTPUT
           mkdir -p "output/$artifact"
-          mv "openslide-winbuild-${{ inputs.pkgver }}.zip" "output/$artifact"
+          mv "openslide-bin-${{ inputs.pkgver }}.tar.gz" "output/$artifact"
           if [ -d override/openslide ]; then
               suffix=$(git -C override/openslide rev-parse HEAD | cut -c-7)
               echo "version_suffix=$suffix" >> $GITHUB_OUTPUT
@@ -141,15 +143,15 @@ jobs:
     runs-on: ubuntu-latest
     container: ${{ inputs.windows_builder_repo_and_digest }}
     steps:
-      - name: Download source zip
+      - name: Download source tarball
         uses: actions/download-artifact@v3
         with:
           name: ${{ needs.sdist.outputs.artifact }}
-      - name: Unpack source zip
+      - name: Unpack source tarball
         run: |
           (cd "${{ needs.sdist.outputs.artifact }}" &&
-              unzip "openslide-winbuild-${{ inputs.pkgver }}.zip")
-          mv "${{ needs.sdist.outputs.artifact }}/openslide-winbuild-${{ inputs.pkgver }}"/* .
+              tar xf "openslide-bin-${{ inputs.pkgver }}.tar.gz")
+          mv "${{ needs.sdist.outputs.artifact }}/openslide-bin-${{ inputs.pkgver }}"/* .
           rm -r "${{ needs.sdist.outputs.artifact }}"
       - name: Download overrides
         if: inputs.openslide_repo != '' || inputs.openslide_java_repo != ''

.gitignore
@@ -1,6 +1,5 @@
 64
 override
-openslide-win*
 openslide-bin-*
 
 __pycache__

artifacts/meson.build
@@ -25,6 +25,10 @@ else
   env.set('OBJDUMP', find_program('objdump').full_path())
 endif
 
+meson.add_dist_script(
+  files('postprocess-sdist.py'), '-i', mesonintrospect
+)
+
 artifacts = [
   custom_target(
     command : [find_program('write-project-versions.py'), '-o', '@OUTPUT@'],

build.sh
@@ -174,42 +174,29 @@ build() {
 
 sdist() {
     # Build source distribution
-    local package file zipdir
-    zipdir="openslide-winbuild-${pkgver}"
-    rm -rf "${zipdir}"
-    meson subprojects download
-    mkdir -p "${zipdir}/subprojects/packagecache"
-    for package in $packages
-    do
-        cp "subprojects/$(echo $package | tr _ -).wrap" "${zipdir}/subprojects/"
-        for file in $(meson_wrap_key $package wrap-file source_filename) \
-                $(meson_wrap_key $package wrap-file patch_filename); do
-            cp "subprojects/packagecache/$file" \
-                    "${zipdir}/subprojects/packagecache/"
-        done
-        for file in $(meson_wrap_key $package wrap-file diff_files | tr , " "); do
-            mkdir -p "${zipdir}/subprojects/packagefiles"
-            cp "subprojects/packagefiles/$file" \
-                    "${zipdir}/subprojects/packagefiles/"
-        done
-    done
-    mkdir -p "${zipdir}"/builder/{linux,windows} \
-            "${zipdir}"/{artifacts,common,deps,machines,utils}
-    cp build.sh README.md CHANGELOG.md COPYING.LESSER meson.build \
-            meson.options "${zipdir}/"
-    cp artifacts/{get-introspect-command,postprocess-binary,write-bdist,write-import-library,write-licenses,write-project-versions}.py \
-            artifacts/meson.build "${zipdir}/artifacts/"
-    cp builder/linux/Dockerfile "${zipdir}/builder/linux/"
-    cp builder/windows/{Dockerfile,package.accept_keywords,package.use,repos.conf} \
-            "${zipdir}/builder/windows/"
-    cp common/{__init__,archive,argparse,meson,software}.py "${zipdir}/common/"
-    cp machines/{cross-{macos-{arm64,x86_64},windows-x64},native-linux-x86_64}.ini \
-            "${zipdir}/machines/"
-    cp deps/{meson.build,setjmp.h} "${zipdir}/deps/"
-    cp utils/get-version.py "${zipdir}/utils/"
-    rm -f "${zipdir}.zip"
-    zip -r "${zipdir}.zip" "${zipdir}"
-    rm -r "${zipdir}"
+    local glib_dir
+    meson subprojects purge --confirm >/dev/null
+    if [ ! -f "${sbuild}/compile_commands.json" ]; then
+        # If the sbuild directory exists, setup didn't complete last time,
+        # and will fail again unless we delete the directory.
+        rm -rf "${sbuild}"
+    fi
+    meson setup \
+            --cross-file "${cross_file}" \
+            "$sbuild" \
+            --reconfigure \
+            -Dall_subprojects=true
+    tag_cachedir 64
+    # manually promote gvdb source to avoid 'meson dist' failure
+    # https://github.com/mesonbuild/meson/issues/12489
+    # delete and recreate for consistency
+    rm -rf subprojects/gvdb
+    glib_dir="$(meson_wrap_key glib wrap-file directory)"
+    cp -a "subprojects/${glib_dir}/subprojects/gvdb" subprojects/
+    # avoid spurious complaints about a dirty work tree
+    git update-index -q --refresh
+    meson dist -C "$sbuild" --formats gztar --include-subprojects --no-tests
+    cp "${sbuild}/meson-dist/openslide-bin-${pkgver}.tar.gz" .
 }
 
 bdist() {
@@ -248,12 +235,16 @@ clean() {
         done
         rm -rf "${build}"
         grep -Flx "[wrap-redirect]" subprojects/*.wrap | xargs -r rm
-        meson subprojects purge --confirm >/dev/null
+        if [ ! -e version ]; then
+            meson subprojects purge --confirm >/dev/null
+        fi
     else
         echo "Cleaning..."
-        rm -rf 64 openslide-bin-*.zip openslide-win*-*.zip
+        rm -rf 64 openslide-bin-*.{tar.gz,zip}
         grep -Flx "[wrap-redirect]" subprojects/*.wrap | xargs -r rm
-        meson subprojects purge --confirm >/dev/null
+        if [ ! -e version ]; then
+            meson subprojects purge --confirm >/dev/null
+        fi
     fi
 }
 
@@ -292,6 +283,7 @@ probe() {
     fi
     export OPENSLIDE_BIN_VERSION="${pkgver}"
 
+    sbuild=64/sdist
     build=64/build
     cross_file="machines/cross-windows-x64.ini"
 }

common/meson.py
@@ -54,4 +54,12 @@ def parse_ini_file(path: Path) -> configparser.RawConfigParser:
 
 
 def default_version() -> str:
-    return date.today().strftime('%Y%m%d') + '-local'
+    # try the version pinned by 'meson dist'
+    try:
+        ver = (meson_source_root() / 'version').read_text().strip()
+        # append "-local" if missing
+        if '-local' not in ver:
+            ver += '-local'
+        return ver
+    except FileNotFoundError:
+        return date.today().strftime('%Y%m%d') + '-local'

deps/meson.build
@@ -1,9 +1,13 @@
+# should we enable all subprojects?  this is needed when building source
+# distributions so we don't leave anything out
+force_subprojects = get_option('all_subprojects')
+
 add_global_arguments(
   '-I' + meson.current_source_dir(),
   language: ['c', 'cpp']
 )
 
-if system == 'darwin'
+if system == 'darwin' and not force_subprojects
   cc = meson.get_compiler('c')
   # [dependency name, library name, version] or
   # [dependency name, library name, header with version, version define]
@@ -53,7 +57,7 @@ else
   subproject('sqlite3')
 endif
 
-if system != 'linux'
+if system != 'linux' or force_subprojects
   subproject('proxy-libintl')
 endif
 

commit 4b5306c706fc7fcfa567a23722056a746ba5109e

find-container-digest: fix invoking from other repos
If we export-ignore the action, it can't be used from other repos.
Continue export-ignoring other children of .github, to keep raw exports
(e.g. GitHub source Zips) relatively clean, and then remove the action
from postprocess-sdist.py.

Fixes: c8c9c063a2 ("Generate sdist archive with `meson dist`")
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

.gitattributes
@@ -2,9 +2,12 @@
 *.sh		eol=lf
 
 # Don't include Git/GitHub metadata and development tool configs in
-# sdist tarballs
+# sdist tarballs.
+# Don't ignore the find-container-digest composite action; remove it from
+# postprocess-sdist.py.  This is necessary so other repos can use the action.
 .gitattributes			export-ignore
 .gitignore			export-ignore
-/.github			export-ignore
+/.github/ISSUE_TEMPLATE		export-ignore
+/.github/workflows		export-ignore
 /.pre-commit-config.yaml	export-ignore
 /pyproject.toml			export-ignore

artifacts/postprocess-sdist.py
@@ -22,6 +22,7 @@ from __future__ import annotations
 
 import os
 from pathlib import Path
+import shutil
 import sys
 
 # handle our own PYTHONPATH prepending, since meson can't set environment
@@ -46,6 +47,9 @@ args.add_arg(
 args.parse()
 os.environ['MESONINTROSPECT'] = args.introspect
 dest = Path(os.environ['MESON_DIST_ROOT'])
+
+# remove those parts of .github not ignored from .gitattributes
+shutil.rmtree(dest / '.github')
 
 # pin openslide-bin version suffix
 version: str = meson_introspect('projectinfo')['version']