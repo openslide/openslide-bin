openslide/openslide-bin

commit d454607c2fb3f79e6b07517ee1f7a0c57353f702

Generate bdist archive entirely through Meson
Include support for building Linux and macOS tar.xz archives.  Change the
filename of the Windows ZIP to fully spell out the OS and CPU
architecture, and to place the version number before the platform name.
Add a machine file property specifying the platform name for use in the
archive filename.

By generating the archive inside Meson, we can avoid running
`meson install` and copying files out of the resulting directory tree.
Remove install configuration for artifacts we've generated.

Since we're not installing, we need to handle operations that Meson would
normally perform at install time, such as updating RPATHs (in
postprocess-binary.py), creating compatibility symlinks to libraries (in
write-bdist.py), and copying the import library (in
write-import-library.py).  We also need to extract the external headers
from the OpenSlide subproject, which is supported via the existing
openslide-bdist.patch.

Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

common/archive.py
@@ -1,0 +1,162 @@
+#
+# Tools for building OpenSlide and its dependencies
+#
+# Copyright (c) 2023 Benjamin Gilbert
+# All rights reserved.
+#
+# This script is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License, version 2.1,
+# as published by the Free Software Foundation.
+#
+# This script is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this script. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+import os
+from pathlib import Path, PurePath
+import re
+import tarfile
+import time
+from types import TracebackType
+from typing import BinaryIO, Self
+import zipfile
+
+
+@dataclass
+class Member(ABC):
+    path: PurePath
+
+
+@dataclass
+class FileMember(Member):
+    fh: BinaryIO
+
+
+@dataclass
+class DirMember(Member):
+    pass
+
+
+@dataclass
+class SymlinkMember(Member):
+    target: PurePath
+
+
+class ArchiveWriter(ABC):
+    def __init__(self, path: Path):
+        self.base = PurePath(re.sub('\\.(tar\\.xz|zip)$', '', path.name))
+        self._members: dict[PurePath, Member] = {}
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        self.close()
+
+    @abstractmethod
+    def close(self) -> None:
+        pass
+
+    def add(self, member: Member) -> None:
+        assert member.path not in self._members
+        self._members[member.path] = member
+        path = member.path.parent
+        while path != PurePath('.'):
+            parent = self._members.setdefault(path, DirMember(path))
+            assert isinstance(parent, DirMember)
+            path = path.parent
+
+    def add_tree(self, arcdir: PurePath, path: Path) -> None:
+        def walkerr(e: OSError) -> None:
+            raise e
+
+        for dpath_str, _, fnames in os.walk(path, onerror=walkerr):
+            dpath = Path(dpath_str)
+            for fname in fnames:
+                self.add(
+                    FileMember(
+                        arcdir / dpath.relative_to(path.parent) / fname,
+                        open(dpath / fname, 'rb'),
+                    )
+                )
+
+
+class TarArchiveWriter(ArchiveWriter):
+    def __init__(self, fh: BinaryIO):
+        super().__init__(Path(fh.name))
+        self._tar = tarfile.open(
+            fileobj=fh,
+            mode='w:xz',
+            format=tarfile.PAX_FORMAT,
+            preset=9,  # type: ignore[call-arg]
+        )
+        self._now = int(time.time())
+
+    def close(self) -> None:
+        for _, member in sorted(self._members.items()):
+            if isinstance(member, FileMember):
+                info = self._tar.gettarinfo(
+                    arcname=member.path.as_posix(), fileobj=member.fh
+                )
+                info.mode = info.mode & ~0o022 | 0o644
+                info.uid = 0
+                info.gid = 0
+                info.uname = 'root'
+                info.gname = 'root'
+                self._tar.addfile(info, member.fh)
+            elif isinstance(member, DirMember):
+                info = tarfile.TarInfo(member.path.as_posix())
+                info.mtime = self._now
+                info.mode = 0o755
+                info.type = tarfile.DIRTYPE
+                info.uname = 'root'
+                info.gname = 'root'
+                self._tar.addfile(info)
+            elif isinstance(member, SymlinkMember):
+                info = tarfile.TarInfo(member.path.as_posix())
+                info.mtime = self._now
+                info.mode = 0o777
+                info.type = tarfile.SYMTYPE
+                info.linkname = member.target.as_posix()
+                info.uname = 'root'
+                info.gname = 'root'
+                self._tar.addfile(info)
+        self._tar.close()
+
+
+class ZipArchiveWriter(ArchiveWriter):
+    def __init__(self, fh: BinaryIO):
+        super().__init__(Path(fh.name))
+        self._zip = zipfile.ZipFile(
+            fh, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9
+        )
+
+    def close(self) -> None:
+        for _, member in sorted(self._members.items()):
+            if isinstance(member, FileMember):
+                try:
+                    info: str | zipfile.ZipInfo = zipfile.ZipInfo.from_file(
+                        member.fh.name, member.path
+                    )
+                except AttributeError:
+                    info = member.path.as_posix()
+                self._zip.writestr(info, member.fh.read())
+            elif isinstance(member, DirMember):
+                self._zip.writestr(member.path.as_posix() + '/', b'')
+            elif isinstance(member, SymlinkMember):
+                raise Exception('Symlinks not supported in Zip')
+        self._zip.close()

.github/workflows/build.yml
@@ -169,7 +169,7 @@ jobs:
           ./build.sh ${suffix:+-s$suffix} -p "${{ inputs.pkgver }}" \
               $werror bdist
           mkdir -p "output/${{ needs.sdist.outputs.artifact }}"
-          mv "openslide-win64-${{ inputs.pkgver }}.zip" \
+          mv "openslide-bin-${{ inputs.pkgver }}-windows-x64.zip" \
               "output/${{ needs.sdist.outputs.artifact }}"
       - name: Upload artifact
         uses: actions/upload-artifact@v3
@@ -188,12 +188,12 @@ jobs:
           name: ${{ needs.sdist.outputs.artifact }}
       - name: Unpack artifact
         shell: bash
-        run: unzip "${{ needs.sdist.outputs.artifact }}/openslide-win64-${{ inputs.pkgver }}.zip"
+        run: unzip "${{ needs.sdist.outputs.artifact }}/openslide-bin-${{ inputs.pkgver }}-windows-x64.zip"
       - name: Report package versions
         shell: bash
-        run: cat "openslide-win64-${{ inputs.pkgver }}/VERSIONS.md" >> $GITHUB_STEP_SUMMARY
+        run: cat "openslide-bin-${{ inputs.pkgver }}-windows-x64/VERSIONS.md" >> $GITHUB_STEP_SUMMARY
       - name: Smoke test
         shell: bash
         run: |
-          cd "${GITHUB_WORKSPACE}/openslide-win64-${{ inputs.pkgver }}/bin"
+          cd "${GITHUB_WORKSPACE}/openslide-bin-${{ inputs.pkgver }}-windows-x64/bin"
           OPENSLIDE_DEBUG=synthetic ./slidetool.exe prop list ""

.github/workflows/release.yml
@@ -63,7 +63,7 @@ jobs:
         env:
           GITHUB_TOKEN: ${{ github.token }}
         run: |
-          unzip "${{ needs.stable.outputs.artifact }}/openslide-win64-${{ needs.setup.outputs.pkgver }}.zip"
+          unzip "${{ needs.stable.outputs.artifact }}/openslide-bin-${{ needs.setup.outputs.pkgver }}-windows-x64.zip"
 
           echo "## Changes" > changes
           awk -e '/^## / && ok {exit}' \
@@ -72,7 +72,7 @@ jobs:
               "openslide-win64-${{ needs.setup.outputs.pkgver }}/CHANGELOG.md" \
               >> changes
           echo -e "## Versions\n" >> changes
-          cat "openslide-win64-${{ needs.setup.outputs.pkgver }}/VERSIONS.md" \
+          cat "openslide-bin-${{ needs.setup.outputs.pkgver }}-windows-x64/VERSIONS.md" \
               >> changes
 
           gh release create --latest --verify-tag \

.gitignore
@@ -1,6 +1,7 @@
 64
 override
 openslide-win*
+openslide-bin-*
 
 __pycache__
 

artifacts/meson.build
@@ -25,9 +25,6 @@ else
   env.set('OBJDUMP', find_program('objdump').full_path())
 endif
 
-fs = import('fs')
-openslide_jar = openslide_java.get_variable('openslide_jar')
-artifact_dir = get_option('prefix') / 'artifacts'
 artifacts = [
   custom_target(
     command : [find_program('write-project-versions.py'), '-o', '@OUTPUT@'],
@@ -35,16 +32,10 @@ artifacts = [
     # ensure we regenerate after dependency updates
     build_always_stale : true,
     env : env,
-    install : true,
-    install_dir : get_option('datadir'),
   ),
-  custom_target(
-    command : ['cp', '@INPUT@', '@OUTPUT@'],
-    input : openslide_jar,
-    output : fs.name(openslide_jar.full_path()),
-    install : true,
-    install_dir : artifact_dir,
-  ),
+  openslide.get_variable('openslide_headers'),
+  openslide_java.get_variable('openslide_jar'),
+  meson.project_source_root() / 'CHANGELOG.md',
 ]
 
 licenses = custom_target(
@@ -53,8 +44,6 @@ licenses = custom_target(
   # ensure we regenerate after dependency updates
   build_always_stale : true,
   env : env,
-  install : true,
-  install_dir : get_option('datadir'),
 )
 artifacts += licenses
 
@@ -67,11 +56,10 @@ if system == 'windows'
     input : libopenslide,
     output : 'libopenslide.lib',
     env : env,
-    install : true,
-    install_dir : artifact_dir,
   )
 endif
 
+fs = import('fs')
 postprocess = find_program('postprocess-binary.py')
 foreach bin : [
   libopenslide,
@@ -84,7 +72,20 @@ foreach bin : [
     input : bin,
     output : [name, name + (system == 'darwin' ? '.dSYM' : '.debug')],
     env : env,
-    install : true,
-    install_dir : artifact_dir,
   )
 endforeach
+
+custom_target(
+  command : [
+    find_program('write-bdist.py'), '-o', '@OUTPUT@', '@INPUT@'
+  ],
+  input : artifacts,
+  output : '@0@-@1@-@2@.@3@'.format(
+    meson.project_name(),
+    meson.project_version(),
+    meson.get_external_property('openslide_bin_platform'),
+    system == 'windows' ? 'zip' : 'tar.xz'
+  ),
+  env : env,
+  build_by_default : true,
+)

artifacts/write-bdist.py
@@ -1,0 +1,103 @@
+#!/usr/bin/env python3
+#
+# Tools for building OpenSlide and its dependencies
+#
+# Copyright (c) 2023 Benjamin Gilbert
+# All rights reserved.
+#
+# This script is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License, version 2.1,
+# as published by the Free Software Foundation.
+#
+# This script is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this script. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from __future__ import annotations
+
+import argparse
+from pathlib import Path, PurePath
+import re
+from typing import BinaryIO
+
+from common.archive import (
+    ArchiveWriter,
+    FileMember,
+    SymlinkMember,
+    TarArchiveWriter,
+    ZipArchiveWriter,
+)
+from common.argparse import TypedArgs
+from common.meson import meson_host
+from common.software import Project
+
+
+class Args(TypedArgs):
+    artifacts: list[Path]
+    output: BinaryIO
+
+
+args = Args('write-bdist', description='Write bdist archive.')
+args.add_arg(
+    '-o',
+    '--output',
+    type=argparse.FileType('wb'),
+    required=True,
+    help='output file',
+)
+args.add_arg(
+    'artifacts',
+    metavar='artifact',
+    nargs='+',
+    type=Path,
+    help='built artifact',
+)
+args.parse()
+
+if meson_host() == 'windows':
+    arc: ArchiveWriter = ZipArchiveWriter(args.output)
+else:
+    arc = TarArchiveWriter(args.output)
+with arc:
+    for path in args.artifacts:
+        name = path.name
+        if re.search(
+            '\\.(lib|(dylib|jnilib)(\\.dSYM)?|so[.0-9]*(\\.debug)?)$', name
+        ):
+            arcdir = arc.base / 'lib'
+        elif name.endswith('.h'):
+            arcdir = arc.base / 'include' / 'openslide'
+        elif name.endswith('.jar'):
+            if meson_host() == 'windows':
+                arcdir = arc.base / 'bin'
+            else:
+                arcdir = arc.base / 'lib'
+        elif name in ('CHANGELOG.md', 'VERSIONS.md', 'licenses'):
+            arcdir = arc.base
+        else:
+            arcdir = arc.base / 'bin'
+
+        if path.is_dir():
+            arc.add_tree(arcdir, path)
+        else:
+            arc.add(FileMember(arcdir / name, path.open('rb')))
+            if re.search('\\.so(\\.[0-9]+){3}$', name):
+                for pat in '(\\.[0-9]+){2}$', '(\\.[0-9]+)+$':
+                    lname = re.sub(pat, '', name)
+                    arc.add(SymlinkMember(arcdir / lname, PurePath(name)))
+            elif re.search('\\.[0-9]+\\.dylib$', name):
+                lname = re.sub('\\.[0-9]+\\.dylib$', '.dylib', name)
+                arc.add(SymlinkMember(arcdir / lname, PurePath(name)))
+
+    # special case: copy OpenSlide README to root
+    arc.add(
+        FileMember(
+            arc.base / 'README.md',
+            open(Project.get('openslide').source_dir / 'README.md', 'rb'),
+        )
+    )

build.sh
@@ -23,10 +23,6 @@ set -eE
 
 packages="zlib libpng libjpeg_turbo libtiff libopenjp2 sqlite3 proxy_libintl libffi pcre2 glib gdk_pixbuf pixman cairo libxml2 uthash libdicom openslide openslide_java"
 
-# Build artifacts
-openslide_artifacts="libopenslide-1.dll slidetool.exe"
-openslide_java_artifacts="openslide-jni.dll openslide.jar"
-
 # Update-checking URLs
 zlib_upurl="https://zlib.net/"
 libpng_upurl="http://www.libpng.org/pub/png/libpng.html"
@@ -174,17 +170,6 @@ build() {
                 ${openslide_werror}
     fi
     meson compile -C "$build" $parallel
-    # When building multiple interdependent subpackages, we need to make sure
-    # the subpackages aren't accessible in the rootdir on subsequent builds,
-    # or else subsequent builds may use a different detection path (system
-    # vs. fallback) than the initial build.  Do this by setting prefix to "/"
-    # and then using --destdir to install into the real rootdir.
-    meson install -C "$build" \
-            --only-changed --no-rebuild --destdir "${root}"
-    # Move OpenSlide Java artifacts to the right place
-    pushd "${root}/lib/openslide-java" >/dev/null
-    cp ${openslide_java_artifacts} "${root}/bin/"
-    popd >/dev/null
 }
 
 sdist() {
@@ -212,13 +197,13 @@ sdist() {
             "${zipdir}"/{artifacts,common,deps,machines,utils}
     cp build.sh README.md CHANGELOG.md COPYING.LESSER meson.build \
             meson.options "${zipdir}/"
-    cp artifacts/{get-introspect-command,postprocess-binary,write-import-library,write-licenses,write-project-versions}.py \
+    cp artifacts/{get-introspect-command,postprocess-binary,write-bdist,write-import-library,write-licenses,write-project-versions}.py \
             artifacts/meson.build "${zipdir}/artifacts/"
     cp builder/linux/Dockerfile "${zipdir}/builder/linux/"
     cp builder/windows/{Dockerfile,package.accept_keywords,package.use,repos.conf} \
             "${zipdir}/builder/windows/"
-    cp common/{__init__,argparse,meson,software}.py "${zipdir}/common/"
-    cp machines/{cross-{macos-{arm64,x86_64},win64},native-linux-x86_64}.ini \
+    cp common/{__init__,archive,argparse,meson,software}.py "${zipdir}/common/"
+    cp machines/{cross-{macos-{arm64,x86_64},windows-x64},native-linux-x86_64}.ini \
             "${zipdir}/machines/"
     cp deps/{meson.build,setjmp.h} "${zipdir}/deps/"
     cp utils/get-version.py "${zipdir}/utils/"
@@ -229,7 +214,7 @@ sdist() {
 
 bdist() {
     # Build binary distribution
-    local package name version srcdir zipdir prev_ver_suffix
+    local prev_ver_suffix
 
     # Rebuild OpenSlide if suffix changed
     prev_ver_suffix="$(cat 64/.suffix 2>/dev/null ||:)"
@@ -248,35 +233,7 @@ bdist() {
         override_remove
     )
 
-    zipdir="openslide-win64-${pkgver}"
-    rm -rf "${zipdir}"
-    mkdir -p "${zipdir}/bin"
-    cp -r CHANGELOG.md "${root}"/share/{licenses,VERSIONS.md} "${zipdir}/"
-    for package in $packages
-    do
-        if [ -d "override/${package}" ] ;then
-            srcdir="override/${package}"
-        else
-            srcdir="subprojects/$(meson_wrap_key ${package} wrap-file directory)"
-        fi
-        for artifact in $(expand ${package}_artifacts)
-        do
-            cp "${root}/artifacts/${artifact}" "${zipdir}/bin/"
-            if [ -f "${root}/artifacts/${artifact}.debug" ]; then
-                cp "${root}/artifacts/${artifact}.debug" "${zipdir}/bin/"
-            fi
-        done
-        if [ "$package" = openslide ]; then
-            mkdir -p "${zipdir}/lib"
-            cp "${root}/artifacts/libopenslide.lib" "${zipdir}/lib/"
-            mkdir -p "${zipdir}/include"
-            cp -r "${root}/include/openslide" "${zipdir}/include/"
-            cp "${srcdir}/README.md" "${zipdir}/"
-        fi
-    done
-    rm -f "${zipdir}.zip"
-    zip -r "${zipdir}.zip" "${zipdir}"
-    rm -r "${zipdir}"
+    cp "${build}/artifacts/openslide-bin-${pkgver}-windows-x64.zip" .
 }
 
 clean() {
@@ -294,7 +251,7 @@ clean() {
         meson subprojects purge --confirm >/dev/null
     else
         echo "Cleaning..."
-        rm -rf 64 openslide-win*-*.zip
+        rm -rf 64 openslide-bin-*.zip openslide-win*-*.zip
         grep -Flx "[wrap-redirect]" subprojects/*.wrap | xargs -r rm
         meson subprojects purge --confirm >/dev/null
     fi
@@ -336,9 +293,7 @@ probe() {
     export OPENSLIDE_BIN_VERSION="${pkgver}"
 
     build=64/build
-    root="$(pwd)/64/root"
-
-    cross_file="machines/cross-win64.ini"
+    cross_file="machines/cross-windows-x64.ini"
 }
 
 fail_handler() {

common/software.py
@@ -40,6 +40,13 @@ class Project:
     primary: bool = False
 
     @staticmethod
+    def get(id: str) -> Project:
+        for p in _PROJECTS:
+            if p.id == id:
+                return p
+        raise KeyError
+
+    @staticmethod
     def get_enabled() -> list[Project]:
         enabled = {
             s['name'] for s in meson_introspect('projectinfo')['subprojects']

commit 5618a3a1c375c1d1a8baaccf9434354e59b1bd12

Fix Zip compression
Directories were being deflated and files were being stored, which is
backwards.  Directories should be stored to avoid a couple bytes of
compression overhead.  File compression was being overridden by the
compress_type field in the ZipInfo object.  Rather than modifying the
object, pass compress_type and compresslevel to ZipFile.writestr().

Fixes: d454607c2f ("Generate bdist archive entirely through Meson")
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

common/archive.py
@@ -141,9 +141,7 @@ class TarArchiveWriter(ArchiveWriter):
 class ZipArchiveWriter(ArchiveWriter):
     def __init__(self, fh: BinaryIO):
         super().__init__(Path(fh.name))
-        self._zip = zipfile.ZipFile(
-            fh, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9
-        )
+        self._zip = zipfile.ZipFile(fh, 'w')
 
     def close(self) -> None:
         for _, member in sorted(self._members.items()):
@@ -154,7 +152,12 @@ class ZipArchiveWriter(ArchiveWriter):
                     )
                 except AttributeError:
                     info = member.path.as_posix()
-                self._zip.writestr(info, member.fh.read())
+                self._zip.writestr(
+                    info,
+                    member.fh.read(),
+                    compress_type=zipfile.ZIP_DEFLATED,
+                    compresslevel=9,
+                )
             elif isinstance(member, DirMember):
                 self._zip.writestr(member.path.as_posix() + '/', b'')
             elif isinstance(member, SymlinkMember):